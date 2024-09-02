76873207e9aa8149d0561303d8978eb7c9badf2b
David Arinze <fivelanes72@gmail.com>
2024-09-02 12:15:23 +0100

    tests

--- config/db.js
+++ config/db.js
@@ -2,14 +2,16 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT || 5432, 
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false, // Disable certificate validation if necessary
+    },
+  },
+});
 
 module.exports = sequelize;
