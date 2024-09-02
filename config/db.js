ae82ea66d17d1d6aad46dedf454368215fadf7e2
David Arinze <fivelanes72@gmail.com>
2024-09-02 12:16:20 +0100

    removed previous ssl connection

--- config/db.js
+++ config/db.js
@@ -2,16 +2,14 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  port: process.env.DB_PORT || 5432, 
-  dialectOptions: {
-    ssl: {
-      require: true, 
-      rejectUnauthorized: false, // Disable certificate validation if necessary
-    },
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  }
+);
 
 module.exports = sequelize;
